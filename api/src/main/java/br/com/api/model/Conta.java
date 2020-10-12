bbe0238c80344874fb8ad7d820e9bb3f2f71504e
jeovane <jeovanebc@gmail.com>
2020-10-12 09:31:08 -0300

    adicionar mapeamento entre Conta e Transferencia

--- api/src/main/java/br/com/api/model/Conta.java
+++ api/src/main/java/br/com/api/model/Conta.java
@@ -1,20 +1,27 @@
 package br.com.api.model;
 
+import br.com.api.exception.SaldoInsuficienteException;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Entity
 @Data
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Conta implements Serializable {
 
     @Id
@@ -25,26 +32,34 @@ public class Conta implements Serializable {
     @Min(value = 0)
     private Double saldo;
 
-    @OneToMany(mappedBy =  "conta", fetch = FetchType.EAGER)
-    @JsonIgnore
-    private List<Transacao> transacao;
+    @NotBlank
+    private String hash;
 
-    public Conta(Long id, Double saldo, List<Transacao> transacao) {
-        this.id = id;
-        this.saldo = saldo;
-        this.transacao = new ArrayList<>();
-    }
+    @OneToMany(mappedBy = "conta", cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private List<Operacao> operacoes;
+
+    @OneToMany(mappedBy = "contaOrigem", cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private List<Transferencia> transferencias;
+
+    public Conta() {}
 
     public Double getSaldo() {
         return Optional.ofNullable(saldo).orElse(0d);
     }
 
-    public void saque(Double valor) {
+    public Double saque(Double valor) {
+        if (!saldoEmaiorOrIgualA(valor)) throw new SaldoInsuficienteException();
         this.setSaldo(this.saldo + valor *-1);
+        return valor;
     }
 
     public void deposito(Double valor) {
         this.setSaldo(this.saldo + valor);
     }
 
+    public boolean saldoEmaiorOrIgualA(Double valor) {
+        return this.saldo >= valor;
+    }
 }
