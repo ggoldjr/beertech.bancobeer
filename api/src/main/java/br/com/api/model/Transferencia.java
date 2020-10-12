5fcc511d9f6b6672d7b2aa6dca1a97afe40dc7ef
jeovane <jeovanebc@gmail.com>
2020-10-12 09:40:25 -0300

    criar a classe Transferencia

---
+++ api/src/main/java/br/com/api/model/Transferencia.java
@@ -0,0 +1,33 @@
+package br.com.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+public class Transferencia implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_hash")
+    private Conta contaOrigem;
+    
+    private String contaDestinoHash;
+
+    private Double valor;
+
+    public Transferencia() {}
+}
