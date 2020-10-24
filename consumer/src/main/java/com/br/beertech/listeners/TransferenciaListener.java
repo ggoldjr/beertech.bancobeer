1cf71f58f09bba25faaab80a00eeafdbf8b0a5be
Geraldo JR <geraldoj@cpqd.com.br>
2020-10-24 17:32:33 -0300

    atualizacao consumer

--- consumer/src/main/java/com/br/beertech/listeners/TransferenciaListener.java
+++ consumer/src/main/java/com/br/beertech/listeners/TransferenciaListener.java
@@ -2,22 +2,31 @@ package com.br.beertech.listeners;
 
 import com.br.beertech.dto.TransferenciaDto;
 import com.br.beertech.messages.TransferenciaMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Optional;
+
 @Component
 public class TransferenciaListener {
 
     private static final Logger logger = LoggerFactory.getLogger(TransferenciaListener.class);
-    private static final String URL = String.format(System.getenv("API_URL") + "transferencias");
+    private static final String URL = String.format(Optional.ofNullable(System.getenv("API_URL")).orElse("http://localhost:8080/")
+                                            + "contas/operacoes/tranferencias");
+
     private final RestTemplate restTemplate;
+    public ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     @Autowired
     public TransferenciaListener(RestTemplate restTemplate) {
@@ -32,7 +41,8 @@ public class TransferenciaListener {
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add("Authorization", auth);
             httpHeaders.add("Content-Type", "application/json");
-            restTemplate.postForObject(URL, transferenciaDto, Void.class, httpHeaders);
+            HttpEntity<String> stringHttpEntity = new HttpEntity<>(mapper.writeValueAsString(transferenciaDto), httpHeaders);
+            restTemplate.exchange(URL, HttpMethod.POST, stringHttpEntity ,Void.class);
         }catch (Exception e){
             logger.error("Error on try request:", e);
         }
