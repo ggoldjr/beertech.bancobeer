c7538b491051634d5049094a3774bb6f9735ea34
jeovane <jeovanebc@gmail.com>
2020-10-25 13:00:12 -0300

    testing the endpoint that update a user password

--- api/src/main/java/br/com/api/dto/AlterarSenhaDto.java
+++ api/src/main/java/br/com/api/dto/AlterarSenhaDto.java
@@ -1,20 +1,25 @@
 package br.com.api.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@With
 public class AlterarSenhaDto {
 
     @NotNull(message = "ID não pode ser nulo.")
+    @NotEmpty(message = "SID não pode ser vazio.")
     private Long idUsuario;
-    @NotNull(message = "Senha antiga não pode ser nulo.")
+    @NotNull(message = "Senha antiga não pode ser nula.")
+    @NotEmpty(message = "Senha antiga não pode ser vazia.")
     private String senhaAntiga;
-    @NotNull(message = "Senha nova não pode ser nulo.")
+    @NotNull(message = "Senha nova não pode ser  nula.")
+    @NotEmpty(message = "Senha nova não pode ser vazia.")
     private String senhaNova;
 }
